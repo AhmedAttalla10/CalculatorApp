1c1fd7845a4ebff237d5dff49754bf0db3f39bc9
AhmedAttalla <ahmedattalla2018@gmail.com>
2020-03-14 18:11:11 -0700

    CalculatorApp

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1: UITextField!
+    @IBOutlet weak var number2: UITextField!
+    @IBOutlet weak var result: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let n1:Double = (Double)(number1.text!)!
+        let n2:Double = (Double)(number2.text!)!
+        var n3 = n1 + n2
+        result.text = (String)(n3)
+    }
+    
 }
 
